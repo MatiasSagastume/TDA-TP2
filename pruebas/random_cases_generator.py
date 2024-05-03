e9cbce951ae0bf036900d614388785a49b8f7ef7
Matias Sagastume <csagastume@fi.uba.ar>
2024-05-03 16:59:55 -0300

    Agregue error en la funcion generadora de casos y agregue casos nuevos

--- pruebas/random_cases_generator.py
+++ pruebas/random_cases_generator.py
@@ -7,14 +7,14 @@ SALTO_DE_LINEA = "\n"
 def random_cases_generator(ruta, max_enemigos, max_recarga, minutos):
     with open(ruta, "w") as archivo:
         archivo.write(PRIMER_LINEA + SALTO_DE_LINEA)
-        archivo.write(str(minutos))
+        archivo.write(f"{minutos}\n")
         for i in range(minutos):
             archivo.write(str(random.randint(0, max_enemigos)) + SALTO_DE_LINEA)
         valor_funcion = random.randint(0, max_recarga)
         for i in range(minutos):
             archivo.write(str(valor_funcion) + SALTO_DE_LINEA)
-            valor_funcion += random.randint(1, max_recarga)
+            valor_funcion += random.randint(0, max_recarga)
 
 
 for i in range(1, 11):
-    random_cases_generator(f"../Casos/CasosNuestros/caso{i}.txt", 10000, 231, 1000)
+    random_cases_generator(f"../Casos/CasosNuestros/caso{i}.txt", 5000, 10, 1000)
